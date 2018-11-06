9275c15a661734c418cfecd1dace01e9e92b0070
Ivankivu <ivankivu@gmail.com>
2018-11-06 15:53:40 +0300

    Add drop down menu
    [delivers #161698387]

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,26 +1,41 @@
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(e) {
+window.onclick = function (e) {
   if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+function upFunction() {
+  document.getElementById("myDropup").classList.toggle("show");
+}
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropup = document.getElementById("myDropup");
+    if (myDropup.classList.contains('show')) {
+      myDropup.classList.remove('show');
+    }
   }
 }
 
 // google map
 
-function initMap() { 
-  var test= {lat: 0.3476, lng: 32.5825}; 
-  var map = new google.maps.Map(document.getElementById('map'), { 
-    zoom: 10, 
-    center: test 
-  }); 
-  var marker = new google.maps.Marker({ 
-    position: test, 
-    map: map 
-  }); 
-} 
+function initMap() {
+  var test = {
+    lat: 0.3476,
+    lng: 32.5825
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: test
+  });
+  var marker = new google.maps.Marker({
+    position: test,
+    map: map
+  });
+}
