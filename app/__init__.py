5fbf7e91097fb876a0715f863e511778536b9982
Ivankivu <ivankivu@gmail.com>
2018-11-17 13:49:48 +0300

    Added db_config.py file for connecting to the database
    [delivers #162019420]

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from app.config import app_config
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object(app_config["development"])
+app.config.from_object(app_config["production"])
 
 from app.api.views.view_users import User
