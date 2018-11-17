92f373ddeb3d4381050b004beffffb0d58d814bf
Ivankivu <ivankivu@gmail.com>
2018-11-17 17:59:39 +0300

    Modified DB_config to set database enviroments testing, production and development
    [delivers #162029913]

--- app/api/tests/base.py
+++ app/api/tests/base.py
@@ -60,5 +60,5 @@ class BaseTestCase(unittest.TestCase):
         with self.client:
             response = self.signup_user("joy", "joy@live.com", "andela1202", "admin")
             self.assertTrue(response.content_type == 'application/json')
-            self.assertEqual(200, response.status_code)
+            self.assertEqual(201, response.status_code)
             self.assertNotEqual(502, response.status_code)
