dd54a2791ec7d81b2db94dd57514addd161130e3
Ivankivu <ivankivu@gmail.com>
2018-11-22 18:54:46 +0300

    Added validations for user input
    [delivers #162149474]

--- app/api/database/db_config.py
+++ app/api/database/db_config.py
@@ -17,19 +17,19 @@ class DBconnect:
             #     self.conn = psycopg2.connect("dbname = 'test_sendit' user ='postgres' host = 'localhost' password = 'postgres' port = '5432'")
             #     self.cursor = self.conn.cursor(cursor_factory=IK.RealDictCursor)
             #     return self.cursor
-            # if app_config['testing']:
-            #     self.conn = psycopg2.connect("dbname = 'travis_ci_test' user ='postgres' host = 'localhost' password = 'postgres' port = '5432'")
-            #     self.cursor = self.conn.cursor(cursor_factory=IK.RealDictCursor)
-            #     return self.cursor
-      
-            # elif app_config['production']:
-            #     self.conn = psycopg2.connect("dbname = 'd6g1ajbujg1285', user = 'lczfiodmgblubu' host = 'ec2-54-235-193-0.compute-1.amazonaws.com' password = '76487fa95068a43af7da7deab9bd4783e1bc97659503032e4b37a6ee5199769c' port = '5432'")
-            # self.cursor = self.conn.cursor(cursor_factory=IK.RealDictCursor)
-            #     return self.cursor
-            if app_config['development']:
-                self.conn = psycopg2.connect("dbname = 'sendit' user = 'postgres' host = 'localhost' password = 'postgres' port = '5432'")
+            if app_config['testing']:
+                self.conn = psycopg2.connect("dbname = 'travis_ci_test' user ='postgres' host = 'localhost' password = 'postgres' port = '5432'")
                 self.cursor = self.conn.cursor(cursor_factory=IK.RealDictCursor)
                 return self.cursor
+      
+            elif app_config['production']:
+                self.conn = psycopg2.connect("dbname = 'd6g1ajbujg1285', user = 'lczfiodmgblubu' host = 'ec2-54-235-193-0.compute-1.amazonaws.com' password = '76487fa95068a43af7da7deab9bd4783e1bc97659503032e4b37a6ee5199769c' port = '5432'")
+                self.cursor = self.conn.cursor(cursor_factory=IK.RealDictCursor)
+                return self.cursor
+            # if app_config['development']:
+            #     self.conn = psycopg2.connect("dbname = 'sendit' user = 'postgres' host = 'localhost' password = 'postgres' port = '5432'")
+            #     self.cursor = self.conn.cursor(cursor_factory=IK.RealDictCursor)
+            #     return self.cursor
 
         except:
             pprint("Cannot connect to the Database!")
